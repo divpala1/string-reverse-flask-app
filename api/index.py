54b4f152c6a5bd5d2ff5f54ea30367eae7135612
Divyajeet Pala <divyajeetpala@gmail.com>
2023-04-13 11:43:40 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
-
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return '''
+        <form action="/reverse" method="post">
+            <input type="text" name="string">
+            <input type="submit" value="Reverse">
+        </form>
+    '''
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/reverse', methods=['POST'])
+def reverse_string():
+    input_string = request.form['string']
+    reversed_string = input_string[::-1]
+    return f'''
+        <p>Original string: {input_string}</p>
+        <p>Reversed string: {reversed_string}</p>
+        <button><a href="/" style="text-decoration: none;">Back</a></button>
+    '''
+
+if __name__ == '__main__':
+    app.run()
